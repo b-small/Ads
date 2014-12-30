fdabe1942133ab4a4b514e6219363122fdd504ef
Blagovest <sblagovesta@yahoo.com>
2014-12-30 13:36:35 +0200

    nothing

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,8 +1,8 @@
 /**
  * Created by user on 12/28/2014.
  */
-adsApp.controller('HomeController', function($scope, homeData) {
-    homeData.getAllAds(function(resp) {
+adsApp.controller('HomeController', function ($scope, homeData) {
+    homeData.getAllAds(function (resp) {
         $scope.data = resp;
     });
 })
